af6a42f37a27b87779c410ada61dd93ebd4ccd83
Dae Euhwa <daedaevibin@naver.com>
2025-06-16 01:53:27 -0500

    fresh start.

--- Lunaviel-Core/astral/celestine.zig
+++
@@ -1,4 +0,0 @@
-// astral/celestine.zig
-pub fn get_gop() ?u32 {
-    return null; // Temporary placeholder
-}
--- Lunaviel-Core/scripts/run-qemu.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/fish
-qemu-system-x86_64 \
-  -drive file=lunaviel.img,format=raw \
-  -bios /usr/share/edk2/x64/OVMF.fd \
-  -serial stdio \
-  -machine q35 \
-  -m 4G \
-  -no-reboot
